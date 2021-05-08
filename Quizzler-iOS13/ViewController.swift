7af0bd754d9eb69ecd5ebc86f32cb6878a9f6e98
Justin Storrer <storrerjustin@gmail.com>
2021-05-07 22:31:07 -0400

    Created a 2D array for quiz questions. Added a IBAction for both "True" and "False" buttons for user to answer the question. Also, added a if statement to let the user know if their answer is correct or not.

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    let quizQuestions = [
+        ["Four + Two is equal to Six", "True"],
+        ["Five - Three is greater than One", "False"],
+        ["Three + Eight is less than Ten", "True"]
+    ]
+    
+    var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
     }
 
-
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle!
+        let actualAnswer = quizQuestions[questionNumber][1]
+        
+        if userAnswer == actualAnswer {
+            print("That's correct!")
+        } else {
+            print("Wrong")
+        }
+        
+        if questionNumber < quizQuestions.count - 1 {
+            questionNumber += 1
+        } else {
+            questionNumber = 0
+        }
+        
+        updateUI()
+        
+    }
+    
+    func updateUI() {
+        questionLabel.text = quizQuestions[questionNumber][0]
+    }
+    
 }
 
